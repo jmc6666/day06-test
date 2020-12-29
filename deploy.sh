75733fda1d5d75e613a3838e526fece8109e41b3
jmc6666 <jmc_xxx@163.com>
2020-12-29 21:04:05 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #docker ps | grep bb
 #[ $? -eq 0 ] && docker rm -f bb
-docker run -d --name flask -p 5000:5000 jmc666/flask:jr
+#docker run -d --name flask -p 5000:5000 jmc666/flask:jr
+docker run -d -p 82:80 nginx
