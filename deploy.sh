cdf32d3884a8955753008e9d0d8a54d7c125d586
jmc6666 <jmc_xxx@163.com>
2020-12-31 16:44:15 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#docker ps | grep bb
-#[ $? -eq 0 ] && docker rm -f bb
-#docker run -d --name flask -p 5000:5000 jmc666/flask:jr
-docker run -d -p 82:80 nginx
+docker ps | grep flask
+[ $? -eq 0 ] && docker rm -f flask
+# 启动 flask 服务
+docker run -d --name flask -p 5000:5000 jmc666/flask:jr
+
