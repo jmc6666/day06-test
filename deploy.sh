ebf0ae71f6788d1bb9b8cae002d82ef8a87f229b
jmc6666 <jmc_xxx@163.com>
2020-12-31 17:53:27 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 docker ps | grep flask
 [ $? -eq 0 ] && docker rm -f flask
 # 启动 flask 服务
-docker run -d --name flask -p 5000:5000 192.168.126.132/jmc666/flask:jr
+docker run -d --name flask -p 5000:5000 192.168.126.132/jmc666/flask
 
